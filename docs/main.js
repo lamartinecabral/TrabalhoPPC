3da6d4a918dc07d9839415eb61a1f54c6d403962
lamartinecabral <11859047+lamartinecabral@users.noreply.github.com>
2020-03-28 02:11:54 -0300

    Update main.js

--- docs/main.js
+++ docs/main.js
@@ -1,37 +1,3 @@
-// MISC
-// shuffleArray = function(original){
-// 	let array = original.map(x=>x);
-// 	let i = array.length;
-// 	while(i--){
-// 		let j = ~~(Math.random()*(i+1));
-// 		[array[i],array[j]] = [array[j],array[i]];
-// 	} return array;
-// }
-
-
-// IMPLEMENTACAO DE ESPERA BLOQUEADA E NOTIFYALL
-// var resolves = {};
-// wait = function(info = {}, sleepTime = 30000){
-// 	return new Promise(resolve=>{
-// 		let notifyId = setTimeout(()=>resolve(), sleepTime);
-// 		resolves[notifyId] = resolve;
-// 		info.notifyId = notifyId;
-// 	});
-// }
-// notify = function(info){
-// 	resolves[info.notifyId]();
-// 	clearTimeout(info.notifyId);
-// 	delete resolves[info.notifyId];
-// }
-// notifyAll = function(){
-// 	for(let notifyId in resolves) notify({notifyId});
-// }
-// sleep = function(sleepTime = 0){
-// 	return new Promise(resolve=>{
-// 		setTimeout(()=>resolve(), sleepTime);
-// 	});
-// }
-
 /*
 Um filósofo, quando fica com sede, tenta pegar as bebidas pra
 preparar um drink. Se alguma bebida não estiver disponivel, ele
@@ -60,7 +26,7 @@ outro. Segundo, é preciso verificar que não causará deadlock. Uma
 vez reservadas todas as bebidas necessárias, é uma questão de tempo
 até ter todas elas disponíveis.
 
-Se um filosofo tem uma bebida reservada e ela está na mesa, ela a pega
+Se um filosofo tem uma bebida reservada e ela está na mesa, ele a pega
 e só soltará depois que beber o drink. Uma vez que o filosofo já pegou
 todas as bebidas necessárias, ele libera elas para serem reservadas
 por outro filosofo e entra no estado de BEBENDO. Somente depois
@@ -72,9 +38,6 @@ que terminar de beber é que ele devolve as bebidas para a mesa.
 var GV = []; // GV[v] é uma lista das bebidas que o filosofo v quer reservar
 var GE = []; // GE[u] responde pra qual filosofo a bebida u está reservada
 
-// initGV = function(filo,bebidas){
-// 	for(let bebida of bebidas) GV[filo][bebida] = true;
-// }
 reserva = function(filo,bebida){
 	if(GE[bebida] == filo) return 0;
 	GV[filo][bebida] = true;
@@ -108,13 +71,14 @@ findDeadLock = function(vertice){ // BFS pra procurar ciclo
 }
 
 // VARIAVEIS DO AMBIENTE
-var contador = 0; // quantos instancias estao em execucao
+var contador = 0; // quantas instancias estao em execucao
 var iterations = 0; // vezes que cada filosofo deve beber
 var locked = []; // marcador para exclusao mutua de bebidas
 var G = []; // lista de bebidas de cada filosofo
 var V; // quantidade de filosofos
 var E; // quantidade de bebidas
 var tempos = []; // contador de tempo em cada estado
+var thread = []; // array de threads de filosofos
 var inicioExecucao;
 
 showStatistics = function(){
@@ -128,58 +92,52 @@ pegouTodas = function(filo,bebidas){
 	for(let b of bebidas) if(locked[b]!=filo) return false; return true;
 }
 
-// THREAD QUE REPRESENTA UM FILOSOFO
-// filosofo = async function(filosofoId){
-// 	for(let iteration=0; iteration<iterations; iteration++){
+
+listenToFilosopher = function(e){
+	
+	let id = e.data.id;
+
+	if(e.data.key == "lock"){
+		// get sortedBebidas
 		
-// 		// ESTADO TRANQUILO
-// 		let tempoTranquilo = ~~(Math.random()*2000);
-// 		await sleep(tempoTranquilo);
-// 		tempos[filosofoId][0] += tempoTranquilo;
-// 		console.log("tranquilo = filosofo "+filosofoId+" por "+tempoTranquilo+"ms");
+		let sortedBebidas = e.data.value;
+		for(let bebida of sortedBebidas){
+			if(reserva(id,bebida)){
+				// se o filosofo pegando essa bebida gera deadlock, ele nao pega
+				if(findDeadLock(id))
+					libera(id,bebida);
+				else
+					if(locked[bebida] == -1) locked[bebida] = id;
+			}
+			else if(GE[bebida] == id && locked[bebida] == -1)
+				locked[bebida] = id;
+		}
 		
-// 		// ESTADO COM SEDE
-// 		let inicioSede = new Date();
-// 		let quantasBebidas = ~~(Math.random()*(G[filosofoId].length-1))+2;
-// 		let sortedBebidas = shuffleArray(G[filosofoId]).slice(0,quantasBebidas);
+		thread[id].postMessage({key: "lock", value: pegouTodas(id,sortedBebidas)});
 		
-// 		// SE ALGUMA BEBIDA INDISPONIVEL, VAI PRA ESPERA BLOQUEADA
-// 		initGV(filosofoId, sortedBebidas);
-// 		while(true){
-// 			for(let bebida of sortedBebidas){
-// 				if(reserva(filosofoId,bebida)){
-// 					// se o filosofo pegando essa bebida gera deadlock, ele desiste de pegar
-// 					if(findDeadLock(filosofoId))
-// 						libera(filosofoId,bebida);
-// 					else
-// 						if(locked[bebida] == -1) locked[bebida] = filosofoId;
-// 				}
-// 				else if(GE[bebida] == filosofoId && locked[bebida] == -1)
-// 					locked[bebida] = filosofoId;
-// 			}
-// 			if(pegouTodas(filosofoId,sortedBebidas)) break;
-// 			await wait();
-// 		}
-// 		// como as bebidas estarao livres em 1seg
-// 		// ja pode liberar elas no grafo do deadlock
-// 		liberarBebidas(filosofoId, sortedBebidas);
+	} else
+	if(e.data.key == "liberarBebidas"){
+		// get sortedBebidas
 		
-// 		let tempoSede = (new Date()) - inicioSede;
-// 		tempos[filosofoId][1] += tempoSede;
-// 		console.log("comSede = filosofo "+filosofoId+" por "+tempoSede+"ms");
+		liberarBebidas(id, e.data.value);
 		
-// 		// ESTADO BEBENDO
-// 		tempos[filosofoId][2] += 1000;
-// 		console.log(filosofoId+" BEBENDO");
-// 		await sleep(1000);
+	} else
+	if(e.data.key == "unlock"){
+		// get sortedBebidas
 		
-// 		// DESBLOQUEIA AS BEBIDAS E NOTIFICA TODOS QUE ESTIVEREM AGUARDANDO
-// 		unlock(filosofoId,sortedBebidas);
-// 		notifyAll();
-// 	}
-// 	console.log("filosofo "+filosofoId+" terminou.");
-// 	if(--contador == 0){ showStatistics(); document.querySelector("#button").disabled = false; }
-// }
+		unlock(id, e.data.value);
+		for(let i = 0; i<V; i++) thread[i].postMessage({key: "wake"});
+		
+	} else
+	if(e.data.key == "sendStatistics"){
+		// get tempos[3]
+		
+		tempos[id] = e.data.value;
+		if(--contador == 0){ showStatistics(); document.querySelector("#button").disabled = false; }
+		
+	}
+}
+
 
 run = function(){
 	document.querySelector("#button").disabled = true;
@@ -189,6 +147,7 @@ run = function(){
 	grafo = grafo.split('\n').filter(x=>x.length).filter(x=>x[0]=="0"||x[0]=="1").map(x=>x.split("//").filter(x=>x[0]=="0"||x[0]=="1").join().split(", ").join(""));
 	console.log("Matriz de adjacencias",grafo);
 	
+	thread = [];
 	V = grafo.length;
 	locked = [];
 	G = []; tempos = []; GV = [];
@@ -205,58 +164,12 @@ run = function(){
 	console.log("Lista de adjacencias Filosofo->Bebidas", G);
 
 	inicioExecucao = new Date();
-	let thread = [];
 	for(let filo = 0; filo < V; filo++){
 		contador++;
-		//filosofo(filo);
-		// initGV(filo, G[filo]);
 		thread[filo] = new Worker("filosofo.js");
-		thread[filo].onmessage = function(e){
-			//console.log(e, "id "+filo);
-			let id = e.data.id;
-
-			if(e.data.key == "lock"){
-				// get sortedBebidas
-				
-				let sortedBebidas = e.data.value;
-				for(let bebida of sortedBebidas){
-					if(reserva(id,bebida)){
-						// se o filosofo pegando essa bebida gera deadlock, ele nao pega
-						if(findDeadLock(id))
-							libera(id,bebida);
-						else
-							if(locked[bebida] == -1) locked[bebida] = id;
-					}
-					else if(GE[bebida] == id && locked[bebida] == -1)
-						locked[bebida] = id;
-				}
-				
-				thread[id].postMessage({key: "lock", value: pegouTodas(id,sortedBebidas)});
-				
-			} else
-			if(e.data.key == "liberarBebidas"){
-				// get sortedBebidas
-				
-				liberarBebidas(id, e.data.value);
-				
-			} else
-			if(e.data.key == "unlock"){
-				// get sortedBebidas
-				
-				unlock(id, e.data.value);
-				for(let i = 0; i<V; i++) thread[i].postMessage({key: "wake"});
-				
-			} else
-			if(e.data.key == "sendStatistics"){
-				// get tempos[3]
-				
-				tempos[id] = e.data.value;
-				if(--contador == 0){ showStatistics(); document.querySelector("#button").disabled = false; }
-				
-			}
-		}
+		thread[filo].onmessage = listenToFilosopher;
 		thread[filo].postMessage({key: "run", value: [ filo, iterations, G[filo] ] });
 	}
 	console.log(GV,GE);
-	console.log("running version 1.0");
+	console.log("running version 1.1");
 }
